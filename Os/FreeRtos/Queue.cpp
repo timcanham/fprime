72d249c0181e7f630ef66b16f9fb2704b134ed51
Timothy Canham <timothy.canham@jpl.nasa.gov>
2019-04-09 22:45:06 -0700

    Finished FreeRtos message queue

--- Os/FreeRtos/Queue.cpp
+++ Os/FreeRtos/Queue.cpp
@@ -6,6 +6,12 @@
 
 namespace Os {
 
+    struct qInfo {
+        QueueHandle_t handle;
+        NATIVE_UINT_TYPE depth;
+        NATIVE_UINT_TYPE msgSize;
+    };
+
     Queue::Queue() :
         m_handle(0) {
     }
@@ -18,7 +24,12 @@ namespace Os {
           return QUEUE_UNINITIALIZED;
         }
 
-        this->m_handle = (POINTER_CAST) queueHandle;
+        qInfo *q_ptr = new qInfo;
+        q_ptr->handle = queueHandle;
+        q_ptr->depth = depth;
+        q_ptr->msgSize = msgSize;
+
+        this->m_handle = (POINTER_CAST) q_ptr;
         
         Queue::s_numQueues++;
 
@@ -26,6 +37,11 @@ namespace Os {
     }
 
     Queue::~Queue() {
+        qInfo *q_ptr = (qInfo*) this->m_handle;
+        if (q_ptr) {
+            vQueueDelete(q_ptr->handle);
+            delete q_ptr;
+        }
     }
 
     Queue::QueueStatus Queue::send(const U8* buffer, NATIVE_INT_TYPE size, NATIVE_INT_TYPE priority, QueueBlocking block) {
@@ -34,17 +50,23 @@ namespace Os {
             return QUEUE_EMPTY_BUFFER;
         }
 
-        QueueHandle_t* queueHandle = (QueueHandle_t*) this->m_handle;
+        if (NULL == this->m_handle) {
+            return QUEUE_UNINITIALIZED;
+        }
 
-        BaseType_t stat;
+        qInfo *q_ptr = (qInfo*) this->m_handle;
+
+        BaseType_t stat = errQUEUE_FULL;
 
         if (Os::Task::isISR()) {
-            stat = xQueueSendFromISR( queueHandle, buffer, 0 ); // don't block in ISR
+            stat = xQueueSendFromISR( q_ptr->handle, buffer, 0 ); // don't block in ISR
         } else {
             if (QUEUE_BLOCKING == block) {
-                stat = xQueueSend( queueHandle, buffer, 10 );
+                while (errQUEUE_FULL == stat) {
+                    stat = xQueueSend( q_ptr->handle, buffer, 10 );
+                }
             } else {
-                stat = xQueueSend( queueHandle, buffer, 0 );
+                stat = xQueueSend( q_ptr->handle, buffer, 0 );
             }
         }
 
@@ -57,112 +79,69 @@ namespace Os {
     }
 
     Queue::QueueStatus Queue::receive(U8* buffer, NATIVE_INT_TYPE capacity, NATIVE_INT_TYPE &actualSize, NATIVE_INT_TYPE &priority, QueueBlocking block) {
-#if 0
 
-        QueueHandle* queueHandle = (QueueHandle*) this->m_handle;
-        mqd_t handle = queueHandle->handle;
-        pthread_cond_t* queueNotEmpty = &queueHandle->queueNotEmpty;
-        pthread_cond_t* queueNotFull = &queueHandle->queueNotFull;
-        pthread_mutex_t* mp = &queueHandle->mp;
+        if (NULL == buffer) {
+            return QUEUE_EMPTY_BUFFER;
+        }
 
-        if (-1 == handle) {
+        if (NULL == this->m_handle) {
             return QUEUE_UNINITIALIZED;
         }
 
-        ssize_t size;
-        int ret; 
-        bool notFinished = true;
-        while (notFinished) {
-            size = mq_receive(handle, (char*) buffer, (size_t) capacity,
-#ifdef TGT_OS_TYPE_VXWORKS        		
-                        (int*)&priority);
-#else
-                        (unsigned int*) &priority);
-#endif        
+        qInfo *q_ptr = (qInfo*) this->m_handle;
 
-            if (-1 == size) { // error
-                switch (errno) {
-                    case EINTR:
-                        continue;
-                    case EMSGSIZE:
-                        return QUEUE_SIZE_MISMATCH;
-                    case EAGAIN:
-                        if (block == QUEUE_NONBLOCKING) {
-                            // no more messages. If we are
-                            // non-blocking, return
-                            return QUEUE_NO_MORE_MSGS;
-                        } else {
-                            // Go to sleep until we receive a signal that something was put on the queue:
-                            // Note: pthread_cont_wait must be called "with mutex locked by the calling 
-                            // thread or undefined behavior results." - from the docs
-                            ret = pthread_mutex_lock(mp);
-                            FW_ASSERT(ret == 0, errno);
-                            ret = pthread_cond_wait(queueNotEmpty, mp);
-                            FW_ASSERT(ret == 0, ret); // If this fails, something horrible happened.
-                            ret = pthread_mutex_unlock(mp);
-                            FW_ASSERT(ret == 0, errno);
-                            continue;
-                        }
-                        break;
-                    default:
-                        return QUEUE_UNKNOWN_ERROR;
-                }
+        BaseType_t stat = errQUEUE_FULL;
+
+        if (QUEUE_BLOCKING == block) {
+            while (errQUEUE_FULL == stat) {
+                stat = xQueueReceive(q_ptr->handle, (void*)buffer, portMAX_DELAY );
             }
-            else {
-                notFinished = false;
-                // Wake up a thread that might be waiting on the other end of the queue:
-                ret = pthread_cond_signal(queueNotFull);
-                FW_ASSERT(ret == 0, ret); // If this fails, something horrible happened.
-            }
+        } else {
+            stat = xQueueReceive(q_ptr->handle, (void*)buffer, 0 );
         }
 
-        actualSize = (NATIVE_INT_TYPE) size;
-#endif
-        return QUEUE_OK;
+        if (pdTRUE == stat) {
+            return QUEUE_OK;
+        } else {
+            return QUEUE_NO_MORE_MSGS;
+        }
+
     }
 
     NATIVE_INT_TYPE Queue::getNumMsgs(void) const {
-#if 0
-        QueueHandle* queueHandle = (QueueHandle*) this->m_handle;
-        mqd_t handle = queueHandle->handle;
 
-        struct mq_attr attr;
-        int status = mq_getattr(handle, &attr);
-        FW_ASSERT(status == 0);
-        return (U32) attr.mq_curmsgs;
-#endif
-        return 0;
+        if (NULL == this->m_handle) {
+            return 0;
+        }
+
+        qInfo *q_ptr = (qInfo*) this->m_handle;
+        return uxQueueMessagesWaiting(q_ptr->handle);
+
     }
 
     NATIVE_INT_TYPE Queue::getMaxMsgs(void) const {
-        //FW_ASSERT(0);
+        // FreeRtos doesn't seem to support high watermark
         return 0;
     }
 
     NATIVE_INT_TYPE Queue::getQueueSize(void) const {
-#if 0
-        QueueHandle* queueHandle = (QueueHandle*) this->m_handle;
-        mqd_t handle = queueHandle->handle;
 
-        struct mq_attr attr;
-        int status = mq_getattr(handle, &attr);
-        FW_ASSERT(status == 0);
-        return (U32) attr.mq_maxmsg;
-#endif
-        return 0;
+        if (NULL == this->m_handle) {
+            return 0;
+        }
+
+        qInfo *q_ptr = (qInfo*) this->m_handle;
+        return q_ptr->depth;
     }
 
     NATIVE_INT_TYPE Queue::getMsgSize(void) const {
-#if 0
-        QueueHandle* queueHandle = (QueueHandle*) this->m_handle;
-        mqd_t handle = queueHandle->handle;
 
-        struct mq_attr attr;
-        int status = mq_getattr(handle, &attr);
-        FW_ASSERT(status == 0);
-        return (U32) attr.mq_msgsize;
-#endif
-        return 0;
+        if (NULL == this->m_handle) {
+            return 0;
+        }
+
+        qInfo *q_ptr = (qInfo*) this->m_handle;
+        return q_ptr->msgSize;
     }
 
 }
